59748e1fa4c0cfffe356b48ce072311b7e461c4d
Zyuzko Vladislav <zyuzko142@gmail.com>
2023-06-07 19:02:32 +0600

    add_layout_to_admin_panel

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ function main(): void {
         case ($_SERVER["REQUEST_URI"] === "/") && empty($_SERVER["QUERY_STRING"]) :
             require_once "structural.php";
             break;
+        case ($_SERVER["REQUEST_URI"] === "/admin") :
+            require_once "admin/admin.php";
+            break;
         default: echo "HERE IS" . var_dump($_SERVER, $_GET);
     }
 }
